3ebe1dfbff24c9c259cfe98368a2a28724f34e93
sangnamkim <56857580+sangnamkim67@users.noreply.github.com>
2020-10-08 06:02:21 +0900

    Creating momentum Clone

---
+++ clock.js
@@ -0,0 +1,19 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+
+    clockTitle.innerText = `${hour < 10 ? `0${hour}` : hour} : ${
+        min < 10 ? `0${min}` : min
+    } : ${sec < 10 ? `0${sec}` : sec}`;
+}
+function init() {
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
---
+++ greeting.js
@@ -0,0 +1,41 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const currentValue = input.value;
+        paintGreeting(currentValue);
+        saveName(currentValue);
+    });
+}
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text} :)`;
+}
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+
+    if (currentUser === null) {
+        // input
+        askForName();
+    } else {
+        // Hello username
+        paintGreeting(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init();
